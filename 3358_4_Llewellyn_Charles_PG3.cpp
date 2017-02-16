7c65d84dd40f669b17194ab493b89873c2a54cb3
ChuckMak <cml176@txstate.edu>
2017-02-16 17:42:10 -0600

    Add files via upload
    Driver: Work in progress

---
+++ 3358_4_Llewellyn_Charles_PG3.cpp
@@ -0,0 +1,84 @@
+//  Authors: Robert James Castleberry 7, Cameron Valdez 9, Charles Llewellyn 24
+//  Due Date: 02/22/2017
+//  Programming Assignment Number: 3
+//  Spring 2017 - CS 3358 - 4
+//
+//  Instructor: Husain Gholoom
+//
+//	
+
+
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include "3358_4_Llewellyn_Charles_PG3.h"
+using namespace std;
+
+//prototypes
+void menu ();
+
+int main ()
+{
+	int choice;
+	double input;
+	
+	cout << "Welcome to My Grades APP\n"
+	menu();
+	cout << "Enter your choice -----> ";
+	cin >>
+	switch (choice)		//puts the copied digit into a switch statement
+		{
+            case 1:			//and outputs the number and the english name
+            	{		
+            		
+				}
+                    break;
+			case 2:	
+				{		
+					
+				}
+                    break;
+            case 3:	
+				{
+					
+				}
+                    break;
+            
+            case 4:	
+				{		
+					
+				} 
+                    break;
+            case 5:	
+				{	
+					
+				} 
+                    break;
+            case 6:	
+            	{	
+            		
+				}
+                   	break;
+				
+            case 7:		
+				{		
+					
+				} 
+                    break;
+         
+			case 9:
+				{
+					
+				}
+					break;
+			
+			default:  cout << choice << " is invalid." << endl;		//returns an error message if choice is invalid
+		}		
+}
+
+
+void menu ()
+{
+	cout << ""
+}
