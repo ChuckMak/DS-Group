9ee1580f12719ee4621b5722f133e9528f2e8468
ChuckMak <cml176@txstate.edu>
2017-02-16 18:27:22 -0600

    Add files via upload

--- 3358_4_Llewellyn_Charles_PG3.cpp
+++ 3358_4_Llewellyn_Charles_PG3.cpp
@@ -23,62 +23,55 @@ int main ()
 	int choice;
 	double input;
 	
-	cout << "Welcome to My Grades APP\n"
-	menu();
-	cout << "Enter your choice -----> ";
-	cin >>
-	switch (choice)		
+	cout << "Welcome to My Grades APP\n";
+	do
+	{
+		menu();
+		cout << "Enter your choice -----> ";
+		cin >> choice;
+		
+		if (cin.fail())
+        {
+            cout << "\nERROR CODE #1: INPUT FAILED\n";
+            cin.clear();
+            cin.ignore(80, '\n');
+        }
+		
+		else
 		{
-            case 1:			
-            	{		
-            		
-				}
-                    break;
-			case 2:	
-				{		
-					
-				}
-                    break;
-            case 3:	
-				{
-					
-				}
-                    break;
-            
-            case 4:	
-				{		
-					
-				} 
-                    break;
-            case 5:	
-				{	
-					
-				} 
-                    break;
-            case 6:	
-            	{	
-            		
-				}
-                   	break;
-				
-            case 7:		
-				{		
-					
-				} 
-                    break;
-         
-			case 9:
-				{
-					
-				}
+		
+			switch (choice)		
+			{
+	            case 1:
+	            	cout << "XXXX\n";
+	            	break;		    
+				case 2:	
+	                break;
+	            case 3:
+	                break;
+	            case 4:	
+	                break;
+	            case 5:	
+	                break;
+	            case 6:
+	                break;
+	            case 7:		
+	                break;
+				case 9:
 					break;
-			
-			default:  cout << choice << " is invalid." << endl;		//returns an error message if choice is invalid
-		}		
+				default:  
+					cout << choice << " is invalid." << endl;		//returns an error message if choice is invalid
+					break;
+			}		
+		}
+	}while(choice!= 9);
 }
+		
 
 
 void menu ()
 {
-	cout << ""
+	cout << "1. Set a Quiz Grade\n2. Set a Programming Assignment Grade\n3. Set a Test Grade\n";
+	cout << "4. Show All Quiz Grades\n5. Show All Assignment Grades\n6. Show all Test Grades\n7. Show Overall Grades\n";
+	cout << "9. Terminate program\n";
 }
